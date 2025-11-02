1d7257aeca98d007f0c888234174cf41aa67eec8
hansnam <nammmx1@gmail.com>
2025-11-03 00:14:32 +0700

    cập nhật thay đổi

--- src/main/java/printstore_app/Main.java
+++ src/main/java/printstore_app/Main.java
@@ -3,6 +3,8 @@
 package printstore_app;
 
 import java.sql.Connection;
+import javax.swing.SwingUtilities;
+import ui.MainFrame;
 
 
 public class Main {
@@ -10,6 +12,11 @@ public static void main(String[] args) {
         Connection conn = DBConnection.getConnection();
         if (conn != null) {
             System.out.println("Kết nối thành công tới cơ sở dữ liệu " + Config.DB_NAME);
+            
+            SwingUtilities.invokeLater(() -> {
+                new MainFrame().setVisible(true);
+            });
+            
         } else {
             System.out.println("Kết nối thất bại!");
         }
